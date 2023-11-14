5ebb093029c25502a4df9933c3b138b68012a629
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-11-15 04:09:02 +0600

    added popular menu section

---
+++ src/pages/shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,16 @@
+const MenuItem = ({ item }) => {
+  const { name, recipe, image, price } = item;
+//   console.log(item);
+  return (
+    <div className="flex space-x-2">
+      <img style={{borderRadius: "0 200px 200px 200px"}} className="w-[100px] object-cover" src={image} alt="" />
+      <div>
+        <h3 className="uppercase">{name}------------------</h3>
+        <p>{recipe}</p>
+        <p className="text-[#BB8506]">${price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
