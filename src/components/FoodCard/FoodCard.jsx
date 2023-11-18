be8ad92d192ff60960fdc020f5501503a1bde966
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-11-19 00:44:33 +0600

    loaded data from server

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -6,14 +6,16 @@ const FoodCard = ({ item }) => {
       <figure>
         <img src={image} alt="Shoes" />
       </figure>
-      <p className="absolute right-0 mr-4 mt-4 px-3 bg-slate-900 text-white">
+      <p className="absolute right-0 mr-4 mt-4 px-3 py-[1px] bg-slate-900 text-white">
         ${price}
       </p>
-      <div className="card-body">
+      <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-center">
-          <button className="btn btn-primary">Add to Cart</button>
+          <button className="btn btn-outline bg-slate-100 border-amber-500 text-center border-0 border-b-4 mt-4 mb-10">
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -12,7 +12,7 @@ import { Autoplay, Navigation } from "swiper/modules";
 const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
-    fetch("reviews.json")
+    fetch("http://localhost:5000/reviews")
       .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);
